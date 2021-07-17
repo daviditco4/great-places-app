fbeb150372657df67221d2d9713188be180fcdf0
David Itcovici <daviditco4@gmail.com>
2021-07-16 21:13:44 -0300

    Move constructor code before properties

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -3,15 +3,15 @@ import 'dart:io';
 import 'location.dart';
 
 class Place {
-  final String id;
-  final String title;
-  final Location location;
-  final File image;
-
   const Place({
     required this.id,
     required this.title,
     required this.location,
     required this.image,
   });
+
+  final String id;
+  final String title;
+  final Location location;
+  final File image;
 }
