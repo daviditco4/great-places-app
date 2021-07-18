4c4a2b7e06ccd5ab59e94423d228883e2c64b083
David Itcovici <daviditco4@gmail.com>
2021-07-17 21:19:58 -0300

    Finish image picker widget

--- lib/widgets/other/image_picker.dart
+++ lib/widgets/other/image_picker.dart
@@ -1,16 +1,28 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart' as picker;
 
 class ImagePicker extends StatefulWidget {
-  const ImagePicker({required this.paddingValue});
-  final double paddingValue;
+  const ImagePicker({required this.pickImgCallback, required this.paddingVal});
+  final void Function(File) pickImgCallback;
+  final double paddingVal;
   @override
   _ImagePickerState createState() => _ImagePickerState();
 }
 
 class _ImagePickerState extends State<ImagePicker> {
-  File? _img;
+  File? _image;
+
+  Future<void> _pickImage({required picker.ImageSource source}) async {
+    final imagePicker = picker.ImagePicker();
+    final image = await imagePicker.pickImage(source: source, maxWidth: 1080.0);
+
+    if (image != null) {
+      setState(() => _image = File(image.path));
+      widget.pickImgCallback(_image!);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,19 +42,29 @@ class _ImagePickerState extends State<ImagePicker> {
                   fit: BoxFit.fill,
                 ),
               ),
-              alignment: _img != null ? null : Alignment.center,
-              child: _img == null
+              alignment: _image != null ? null : Alignment.center,
+              child: _image == null
                   ? const Text('No Image Selected', textAlign: TextAlign.center)
-                  : Image.file(_img!,
-                      width: double.infinity, fit: BoxFit.contain),
+                  : Image.file(_image!, fit: BoxFit.contain),
             ),
           ),
         ),
-        SizedBox(width: widget.paddingValue),
-        TextButton.icon(
-          icon: const Icon(Icons.camera),
-          label: const Text('Take Picture'),
-          onPressed: () {},
+        SizedBox(width: widget.paddingVal),
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextButton.icon(
+              icon: const Icon(Icons.camera),
+              label: const Text('Take Picture'),
+              onPressed: () => _pickImage(source: picker.ImageSource.camera),
+            ),
+            TextButton.icon(
+              icon: const Icon(Icons.photo_library),
+              label: const Text('Choose From Gallery'),
+              onPressed: () => _pickImage(source: picker.ImageSource.gallery),
+            ),
+          ],
         ),
       ],
     );
